20b028492f9c9dd33491fe4ee4fa71123232edad
Ubuntu <ubuntu@ip-172-31-42-179.us-east-2.compute.internal>
2025-12-08 15:46:26 +0000

    Updated message.py and minor EC2-side edits

--- message.py
+++ message.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime
 import smtplib
 from email.message import EmailMessage
-from trading_algo_SPY import trading_signal
+
 
 def send_text(message: str):
     msg = EmailMessage()
@@ -19,34 +19,7 @@ def send_text(message: str):
         print("ERROR in send_text:", repr(e))
 
 
-last_signal = None  # store last signal sent
 
-
-while True:
-    signal = trading_signal()
-    now = datetime.utcnow()
-
-    # Only send if the signal CHANGED
-    if signal != last_signal:
-        if signal == "buy it":
-            send_text("BUY SIGNAL: SPY")
-
-        elif signal == "sell current price is less than the 50 day moving average !!!":
-            send_text("SELL SIGNAL: SPY")
-
-        elif signal == "hold it":
-            send_text("HOLD SIGNAL: SPY")
-
-        last_signal = signal  # update the last signal
-
-    # Print so you can see what it's doing
-    print(f"{now} | SIGNAL: {signal}")
-
-    # Wait 10 minutes
-    time.sleep(600)
-
-
- 
 
 
 
