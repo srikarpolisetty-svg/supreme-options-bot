d2fbf8fd392717587693eb17e6aec288321487e7
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2025-12-26 10:42:40 -0500

    test file

--- testfile.py
+++ testfile.py
@@ -1,1 +1,12 @@
+import duckdb
 
+con = duckdb.connect("options_data.db", read_only=True)
+
+
+
+con.execute("""
+    SELECT *
+    FROM option_snapshots_enriched
+    ORDER BY timestamp DESC
+    LIMIT 6;
+""").fetchall()
