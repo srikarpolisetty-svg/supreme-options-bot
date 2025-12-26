dacaf287f91434dd36e28ccc7ce9599e5d581117
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2025-12-26 11:24:41 -0500

    test file

--- testfile.py
+++ testfile.py
@@ -5,9 +5,10 @@ con = duckdb.connect("options_data.db", read_only=True)
 df = con.execute("""
     SELECT *
     FROM option_snapshots_enriched
-    ORDER BY timestamp DESC
+    ORDER BY timestamp ASC
     LIMIT 6
 """).df()
+
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
