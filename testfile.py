a05a16ed79558974043aab22571259d4d8021d71
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2025-12-26 11:32:47 -0500

    test

--- testfile.py
+++ testfile.py
@@ -1,5 +1,17 @@
 import duckdb
 import pandas as pd
+
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+
+
+
+
 con = duckdb.connect("options_data.db", read_only=True)
 
 df = con.execute("""
@@ -10,6 +22,4 @@ df = con.execute("""
 
 
 
-pd.set_option("display.max_columns", None)
-pd.set_option("display.width", None)
 print(df)
